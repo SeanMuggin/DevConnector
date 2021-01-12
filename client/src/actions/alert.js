9e44ce94c216d0145959600a93da232a4a309a84
SeanMuggin <sean@mcginns.com>
2021-01-12 14:34:53 -0800

    Prepare for deployment. UUID v4

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,6 +1,5 @@
 import { SET_ALERT, REMOVE_ALERT } from './types';
-import uuid from 'uuid/v4';
-
+import { v4 as uuid } from 'uuid';
 //dispatch utilizes thunk middleware
 export const setAlert = (msg, alertType, timeout = 5000) => (dispatch) => {
   const id = uuid.v4();
